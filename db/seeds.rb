a774d3e8cafc6f9b5bcfb9a9497e989be73cf885
trieulieuf9 <trieulieuf9@gmail.com>
2016-03-10 20:18:29 +0700

    milestone 4 with Faker from lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+	Picture.create(username: Faker::Name.name, caption: Faker::Lorem.paragraph, 
+		likes_count: Faker::Number.number(3),
+		url: Faker::Avatar.image)
+end
